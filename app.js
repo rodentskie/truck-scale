99b07e8bb89b314b3622b660bdc39515240f0f8c
= <=>
2019-10-09 16:57:28 +0800

    add properties to serial port

--- app.js
+++ app.js
@@ -13,8 +13,11 @@ app.use(express.static("public")); //Send index.html page on GET /
 const SerialPort = require("serialport");
 const Readline = SerialPort.parsers.Readline;
 const port = new SerialPort("COM3", {
-  baudRate: 115200
-}); 
+  baudRate: 115200,
+  parity: "none",
+  dataBits: 8,
+  stopBits: 1
+});
 //Connect serial port to port COM3. Because my Arduino Board is connected on port COM3.
 //See yours on Arduino IDE -> Tools -> Port
 const parser = port.pipe(new Readline({ delimiter: "\r\n" })); //Read the line only when new line comes.
